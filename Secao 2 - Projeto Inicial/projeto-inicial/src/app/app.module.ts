b12fab32cba51604436035e3297f7581efd0e282
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2025-01-17 14:28:21 -0300

    Correção de nome da pasta

---
+++ Secao 2 - Projeto Inicial/projeto-inicial/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Comp2Component } from './comp2/comp2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Comp2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
