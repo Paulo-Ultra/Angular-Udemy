38066bd1626425c7f936a9a36ccded14fee76a83
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2025-01-23 09:24:44 -0300

    Utilizando interpolação

--- Secao 3 - Componentes e Estilizacoes/projeto-components/src/app/card/card.component.ts
+++ Secao 3 - Componentes e Estilizacoes/projeto-components/src/app/card/card.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
 
+  plano = {
+    infos: {
+      tipo: 'Simples',
+      preco: 100
+    }
+  };
+
+  ngOnInit(): void {
+    console.log("OnInit");
+  }
 }
