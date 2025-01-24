f706e11d1375c4e654966f2dc04884572111fe71
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2025-01-24 08:36:29 -0300

    Template dentro do componente (propriedade template)

--- Secao 3 - Componentes e Estilizacoes/projeto-components/src/app/card-button/card-button.component.ts
+++ Secao 3 - Componentes e Estilizacoes/projeto-components/src/app/card-button/card-button.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
-  templateUrl: './card-button.component.html',
+  //templateUrl: './card-button.component.html',
+   //Gera o component com template e não templateUrl
+  //ng g c inline --inline-template
+  template: `
+    <div class="card-button">Teste</div>
+  `,
   styleUrl: './card-button.component.scss'
 })
 export class CardButtonComponent {
