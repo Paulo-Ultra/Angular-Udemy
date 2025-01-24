edc909dcbbc20adf57a8d0e2e47852e5402a0b0d
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2025-01-24 08:52:31 -0300

    Estilos CSS dentro do componente (propriedade styles)

--- Secao 3 - Componentes e Estilizacoes/projeto-components/src/app/card-button/card-button.component.ts
+++ Secao 3 - Componentes e Estilizacoes/projeto-components/src/app/card-button/card-button.component.ts
@@ -2,12 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
-  //templateUrl: './card-button.component.html',
-   //Gera o component com template e não templateUrl
-  //ng g c inline --inline-template
-  template: `
-    <div class="card-button">Teste</div>
-  `,
+  templateUrl: './card-button.component.html',
   styleUrl: './card-button.component.scss'
 })
 export class CardButtonComponent {
