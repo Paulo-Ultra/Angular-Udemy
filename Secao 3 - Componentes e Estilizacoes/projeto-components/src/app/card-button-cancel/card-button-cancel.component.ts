02e291671e9ec50cb6571f35a9acf52459739051
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2025-01-28 21:07:47 -0300

    Styles Inline

--- Secao 3 - Componentes e Estilizacoes/projeto-components/src/app/card-button-cancel/card-button-cancel.component.ts
+++ Secao 3 - Componentes e Estilizacoes/projeto-components/src/app/card-button-cancel/card-button-cancel.component.ts
@@ -3,10 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
-  styleUrls: ['./card-button-cancel.component.scss', './card-button-cancel-2.component.scss',
-    '../card/card.component.scss'
-   ],
+  styleUrls: ['./card-button-cancel.component.scss'],
 })
+
 export class CardButtonCancelComponent {
 
 }
