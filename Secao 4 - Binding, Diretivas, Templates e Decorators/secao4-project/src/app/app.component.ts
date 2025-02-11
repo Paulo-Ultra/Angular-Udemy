80f0f166c0526be854a11f3948db857ce16eb897
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2025-02-11 14:25:49 -0300

    CSS Class Binding

--- Secao 4 - Binding, Diretivas, Templates e Decorators/secao4-project/src/app/app.component.ts
+++ Secao 4 - Binding, Diretivas, Templates e Decorators/secao4-project/src/app/app.component.ts
@@ -6,27 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
-  widthButton1 = '110px';
-  widthButton2 = 130;
-  stylesObj = {
-    width: '160px',
-    backgroundColor: 'green'
-  }
-
-  updateStyleObj() {
-    this.stylesObj.width = '170px';
-    this.stylesObj.backgroundColor = 'grey';
-  }
-
-  updateStyleObjCorrect() {
-    this.stylesObj = {
-      width: '170px',
-      backgroundColor: 'grey'
-    }
-  }
-
-  updateWidth() {
-    this.widthButton2 = 200;
-  }
+  isRedButton = true;
+  isGreen = true;
 }
