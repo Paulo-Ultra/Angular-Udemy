7d5dd053bdfd6d7b8417d00bbf9a3994a841f42e
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2025-02-11 10:55:15 -0300

    Attribute Binding

--- Secao 4 - Binding, Diretivas, Templates e Decorators/secao4-project/src/app/app.component.ts
+++ Secao 4 - Binding, Diretivas, Templates e Decorators/secao4-project/src/app/app.component.ts
@@ -6,40 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  inputText: string = 'Texto Inicial';
-  inputType = "text";
-  isDisabled = false;
 
-  enableInput() {
-    this.isDisabled = false;
-  }
+    buttonTitle = 'Título do botão';
+    buttonDisable = true;
 
-  disableInput() {
-    this.isDisabled = true;
-  }
-
-  setPasswordTypeInput() {
-    this.inputType = 'password';
-  }
-
-  setTextTypeInput(){
-    this.inputType = 'text';
-  }
-
-  logInputText() {
-    console.log(this.inputText);
-  }
-
-  handleInputKeyup(event: KeyboardEvent) {
-    //valor pego da interface que é tipagem correta e fazendo um casting dessa propriedade
-    //Os valores das propriedades ficam na propriedade target
-    const currentText = (event.target as HTMLInputElement).value;
-    console.log(currentText);
-  }
-
-  handleInputEvent(event: Event) {
-    const currentText = (event.target as HTMLInputElement).value;
-    console.log(currentText);
-    //https://v17.angular.io/guide/event-binding-concepts
-  }
+    onButtonClick() {
+      this.buttonTitle = 'Título Alterado';
+      this.buttonDisable = !this.buttonDisable;
+    }
 }
