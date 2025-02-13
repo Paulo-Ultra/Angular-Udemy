154f513a1852facb272f4660ac63900a9460c42d
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2025-02-13 13:37:11 -0300

    Propriedade Transform do @Input

--- Secao 4 - Binding, Diretivas, Templates e Decorators/projeto-components/src/app/card-button/card-button.component.ts
+++ Secao 4 - Binding, Diretivas, Templates e Decorators/projeto-components/src/app/card-button/card-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { booleanAttribute, Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
@@ -12,6 +12,8 @@ export class CardButtonComponent {
   //@Output('buttonClick') buttonClickEmitter: EventEmitter<boolean> = new EventEmitter<boolean>();
   //Forma menos verbosa
   @Output('buttonClick') buttonClickEmitter = new EventEmitter<boolean>();
+
+  @Input({ transform: booleanAttribute }) buttonDisabled: boolean = false;
 
   onButtonClick(){
     console.log('onButtonClick');
--- Secao 4 - Binding, Diretivas, Templates e Decorators/projeto-components/src/app/card/card.component.ts
+++ Secao 4 - Binding, Diretivas, Templates e Decorators/projeto-components/src/app/card/card.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, numberAttribute } from '@angular/core';
+
+//Se a logica for grande ou complexa é ideal usar function para o transform
+function handlePlanType (value: string){
+  console.log('handlePlanType', value);
+  return value.toUpperCase();
+}
 
 @Component({
   selector: 'app-card',
@@ -7,19 +13,10 @@ import { Component, Input } from '@angular/core';
 })
 export class CardComponent {
 
-  //Required é do angular 16+
+  //Required é do angular 16+ //Aula 58 Transform tem functions nativas como a numberAttribute
+  //@Input({ required: true, alias: 'planPriceAlias', transform: numberAttribute }) planPrice: number = 0;
   @Input({ required: true, alias: 'planPriceAlias' }) planPrice: number = 0;
-
-  private _planType: string = '';
-
-  @Input('planType') set planType(value: string) {
-    this._planType = value.toUpperCase();
-  }
-
-  get planType(): string{
-    return this._planType;
-  }
-
+  @Input( { alias: 'planType', transform: (value: string) => handlePlanType(value) }) planType: string = '';
 
   buttonClicked(valueEmmited: boolean){
     console.log('ButtonClicked', valueEmmited);
