469792f9f0b071310068ecbc32b44e5e23b9fc47
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2025-03-05 11:02:36 -0300

    Projeto Filtro Lista de Usuários - Angular Material

---
+++ Secao 5 - Filtro de Lista de Usuários/users-filter-project/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ComponentsModule,
+  ],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
