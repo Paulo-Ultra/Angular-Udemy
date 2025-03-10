8827c4be9f8cbd26c2acff44fb159fb4b7052549
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2025-03-10 13:45:26 -0300

    Criação do Layout do Componente de Tabela

--- Secao 5 - Filtro de Lista de Usuários/users-filter-project/src/app/angular-material/angular-material.module.ts
+++ Secao 5 - Filtro de Lista de Usuários/users-filter-project/src/app/angular-material/angular-material.module.ts
@@ -7,6 +7,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from "@angular/material/core";
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from "@angular/material/button";
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   imports:[
@@ -17,7 +18,8 @@ import { MatButtonModule } from "@angular/material/button";
     MatDatepickerModule,
     MatNativeDateModule,
     MatSelectModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule
   ],
   exports:[
     MatListModule,
@@ -27,7 +29,8 @@ import { MatButtonModule } from "@angular/material/button";
     MatDatepickerModule,
     MatNativeDateModule,
     MatSelectModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule
   ],
 })
 export class AngularMaterialModule {
