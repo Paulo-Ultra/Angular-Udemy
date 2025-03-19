eb43392a4c0fdfefc70bdd0bdbe2b4c659bc04b8
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2025-03-19 10:08:41 -0300

    Criação da Lógica de Filtro por Data e uso da lib date-fns

--- Secao 5 - Filtro de Lista de Usuários/users-filter-project/src/app/app.component.ts
+++ Secao 5 - Filtro de Lista de Usuários/users-filter-project/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
 import { UsersList } from './data/users-list';
 import { IFilterOptions } from './interfaces/filter-options.interface';
+import { isWithinInterval } from 'date-fns';
 
 @Component({
   selector: 'app-root',
@@ -38,7 +39,10 @@ export class AppComponent implements OnInit {
     let filteredList: IUser[] = [];
 
     filteredList = this.filterUsersListByName(filterOptions.name, usersList);
+
     filteredList = this.filterUsersListByStatus(filterOptions.status, filteredList);
+
+    filteredList = this.filterUsersListByDate(filterOptions.startDate, filterOptions.endDate, filteredList);
 
     return filteredList;
   }
@@ -65,5 +69,21 @@ export class AppComponent implements OnInit {
     const filteredList = userList.filter((user) => user.ativo === status);
 
     return filteredList;
+  }
+
+  filterUsersListByDate(startDate: Date | undefined, endDate: Date | undefined, usersList: IUser[]): IUser[] {
+    const DATE_NOT_SELECTED = startDate === undefined || endDate === undefined;
+
+    if(DATE_NOT_SELECTED) {
+      return usersList;
+    }
+
+    const checkDateInterval = (user: IUser) => isWithinInterval(new Date(user.dataCadastro), {
+      start: startDate,
+      end: endDate,
+    });
+
+    const listFiltered = usersList.filter(checkDateInterval);
+      return listFiltered;
   }
 }
