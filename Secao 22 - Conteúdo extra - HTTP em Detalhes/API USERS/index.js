0007c8ed66ae80fbe808ef5e7f2a3a6a0b56daeb
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2025-03-24 17:02:00 -0300

    Método GET(Pelo Browser)

--- Secao 22 - Conteúdo extra - HTTP em Detalhes/API USERS/index.js
+++ Secao 22 - Conteúdo extra - HTTP em Detalhes/API USERS/index.js
@@ -4,8 +4,34 @@ const server = express();
 
 server.listen(3000);
 
+const users = [
+    {
+        id: 0,
+        name: 'Paulo',
+        age: 38
+    }
+];
+
+server.get('/users', (request, response) => {
+    //response.status(200).send(users);
+    response.status(200).json(users);
+});
+
+server.get('/users/:id', (request, response) => {
+    const userId = request.params.id;
+    const user = users.find(user => user.id.toString() === userId);
+    // const user = users.find(user => user.id === +userId); -> Outra solução, mudar o tipo para number
+
+
+    if(user) {
+        response.status(200).send(user);
+    } else {
+        response.status(404).send('Usuário não encontrado.');
+    }    
+});
+
 server.get('/', (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
+    response.setHeader('Content-Type', 'text/html; charset=UTF-8');
     response.sendFile('page.html', { root: __dirname });
 });
 
